81797bd6bb31c2d7b8410138ea310379394033b6
Madhavbansal1 <madhavbansal6633@gmail.com>
2025-02-24 08:58:41 +0530

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
 
-export default function App() {
+function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+function Login() {
+    return (
+        <>
+            <header>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </header>
+        </>
+    )
+}
+
+export default Login
